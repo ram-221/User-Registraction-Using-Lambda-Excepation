8c74ef047e6e3f268cd64769a548175a3cd72dc0
ram-221 <rameshmalli857.com>
2022-08-06 00:46:02 +0530

    using Functinal Excepation code to User entry

---
+++ UserRegistrationException.java
@@ -0,0 +1,9 @@
+package com.lambdaexception;
+
+public class UserRegistrationException extends Exception {
+
+    public UserRegistrationException(String message){
+        super(message);
+    }
+
+}
